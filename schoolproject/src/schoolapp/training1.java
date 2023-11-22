0aaa6f097e0e235293e3cbbc818a633f59026e36
akbar <akbar@DESKTOP-B7M5UT8>
2023-11-21 23:19:01 -0500

    first commit

---
+++ schoolproject/src/schoolapp/training1.java
@@ -0,0 +1,12 @@
+package schoolapp;
+
+public class training1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         double a = 30;
+         int d = (int)a;
+         System.out.println(d);
+	}
+
+}
